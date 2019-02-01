54c3c858e44f8323af2869cc3b5597a13ed42754
renrith <sandrafernfer@gmail.com>
2019-02-01 08:50:56 +0100

    delete footer partial

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class Footer extends Component {
-    render() {
-        return (<footer className="app-footer">Footer</footer>);
-    }
-}
-
-export default Footer;
