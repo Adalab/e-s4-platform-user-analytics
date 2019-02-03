6e44a3251ee49fdbb90cffbd405b5a7622613fa9
carolcesp <carolinacespedes90@gmail.com>
2019-02-03 11:39:07 +0100

    added table list

--- src/components/SessionList.js
+++ src/components/SessionList.js
@@ -3,14 +3,48 @@ import { Link } from 'react-router-dom';
 import Pagination from "react-js-pagination"; 
 import UserFilter from './UserFilter';
 
+
 class SessionList extends Component {
     constructor(props) {
         super(props)
     
         this.state = {
-          activePage: 15
+          activePage: 15,
+            userData: [
+              {
+                "user__username": "jade@stylesage.co",
+                "max_timestamp": "2019-01-28T17:32:49.570Z",
+                "user__job_title": "",
+                "min_timestamp": "2019-01-28T15:20:34.682Z",
+                "duration_sec": 7934.888322,
+                "user": 17,
+                "request_count": 50,
+                "session_key": "4d3z5dnb34rlojytunofjzma0j8nggyq"
+              },
+              {
+                "user__username": "jade@stylesage.co",
+                "max_timestamp": "2019-01-22T15:34:32.548Z",
+                "user__job_title": "",
+                "min_timestamp": "2019-01-22T15:33:26.764Z",
+                "duration_sec": 65.783448,
+                "user": 17,
+                "request_count": 19,
+                "session_key": "dieyd3xze1ovzq4zdgexyt3rqeg4uq6p"
+              },
+              {
+                "user__username": "jade@stylesage.co",
+                "max_timestamp": "2019-01-09T15:08:27.590Z",
+                "user__job_title": "",
+                "min_timestamp": "2019-01-09T14:40:17.034Z",
+                "duration_sec": 1690.556118,
+                "user": 17,
+                "request_count": 106,
+                "session_key": "eiqk9g55mpkg3hq29q1mu5ozkck13ka6"
+              }
+            ]
+          }
            
-        }
+        
         this.handlePageChange = this.handlePageChange.bind(this);
     }
 
@@ -45,24 +79,34 @@ class SessionList extends Component {
                         <UserFilter />
                     </div>
                     <div className="table__container">
-                        <table className="table__container--results">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Username</th>
-                                    <th scope="col">Time Started (local TZ)</th>
-                                    <th scope="col">Duration</th>
-                                    <th scope="col">Request Count</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>*data.username</td>
-                                    <td>*data.time</td>
-                                    <td>*data.duration</td>
-                                    <td>*data.request</td>
-                                </tr>
-                            </tbody>
-                        </table>
+                        <ul className="username__list">UserName
+                            {this.state.userData.map((item,index) => {
+                            return(
+                            <li key={index}>{item.user__username}</li>
+                            )
+                         })}
+                        </ul>
+                        <ul className="username__list">Time
+                            {this.state.userData.map((item,index) => {
+                            return(
+                            <li key={index}>{item.min_timestamp}</li>
+                            )
+                         })}
+                        </ul>
+                        <ul className="username__list">Duration
+                            {this.state.userData.map((item,index) => {
+                            return(
+                            <li key={index}>{item.duration_sec}</li>
+                            )
+                         })}
+                        </ul>
+                        <ul className="username__list">Request Count
+                            {this.state.userData.map((item,index) => {
+                            return(
+                            <li key={index}>{item.request_count}</li>
+                            )
+                         })}
+                        </ul>
                     </div> 
                 </main>
                 <footer className="app__footer--session">
@@ -80,7 +124,7 @@ class SessionList extends Component {
                     </div>
                     <div className="options__pagination">
                         <Pagination
-                        innerClassName={<ul className="pagination__list"></ul>}
+                        innerClassName={<ul className="pagination"></ul>}
                         activeClassName={<li className="pagination__list--item"></li>}
                         firstPageText={<li className="pagination__list--item">First</li>}
                         prevPageText={<li className="pagination__list--item">Previous</li>}
