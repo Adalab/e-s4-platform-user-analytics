2f3202c811750ac142d60a9a07535f68a8a9471c
pixelvega <pixelvega.info@gmail.com>
2019-02-15 09:34:49 +0100

    add margin, padding in table and title

--- src/components/component-list/SessionList.js
+++ src/components/component-list/SessionList.js
@@ -285,7 +285,7 @@ class SessionList extends Component {
                         </ul>
                     </div>
                     <div className="panel__session">
-                        <i className="zmdi zmdi-apps"></i>  
+                        <i className="zmdi zmdi-apps panel__session-icon"></i>  
                         <h2 className="panel__session-title">Sessions between {resultsDate}</h2>
                     </div>
                     <UserFilter getQueryUsername={this.getQueryUsername} resultsNumber={resultsNumber} />
